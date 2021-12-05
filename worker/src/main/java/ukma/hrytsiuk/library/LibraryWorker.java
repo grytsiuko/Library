8dfb2d581a323f9027a430d6d164694b9b30d34d
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2021-12-05 16:33:27 +0200

    Added worker module

---
+++ worker/src/main/java/ukma/hrytsiuk/library/LibraryWorker.java
@@ -0,0 +1,13 @@
+package ukma.hrytsiuk.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWorker {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWorker.class, args);
+	}
+
+}
